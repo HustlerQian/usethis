1375757bc2e9c3483c5e241ccc6dc0bc6b3c081b
HustlerQian <15901612377@126.com>
2017-08-07 12:20:47 +0800

    Add files via upload

--- R/ignore.R
+++ R/ignore.R
@@ -25,5 +25,6 @@ use_build_ignore <- function(files, escape = TRUE, base_path = ".") {
 escape_path <- function(x) {
   x <- gsub("\\.", "\\\\.", x)
   x <- gsub("/$", "", x)
+  x <- gsub("(^\\D:)", "\\1\\\\", x)
   paste0("^", x, "$")
 }
